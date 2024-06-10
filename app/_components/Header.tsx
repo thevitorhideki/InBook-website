4b4f9e3200bc6cbeae899b89447a32d0feb5c27c
Vitor Katakura <vitorhideki@debian.debian.com>
2024-06-10 12:33:15 -0300

    Página inicial fiel ao figma

---
+++ app/_components/Header.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-evenly p-4">
+      <h1 className="text-3xl font-bold">InBook</h1>
+      {/* <Image src="/logo.svg" alt="Logo" width={200} height={200} /> */}
+      <nav>
+        <ul className="flex justify-between gap-8">
+          <li>
+            <a href="#features">Experiências</a>
+          </li>
+          <li>
+            <a href="#plans">Planos</a>
+          </li>
+          <li>
+            <a href="#about">Sobre</a>
+          </li>
+        </ul>
+      </nav>
+      <select
+        name="languages"
+        id="lang"
+        className="rounded-md border-2 border-slate-100 bg-white p-2 focus:outline-none"
+        style={{
+          appearance: "none",
+          WebkitAppearance: "none",
+          MozAppearance: "none",
+        }}
+      >
+        <option value="English">🇬🇧 English</option>
+        <option value="Português">🇧🇷 Português</option>
+      </select>
+    </header>
+  );
+}
